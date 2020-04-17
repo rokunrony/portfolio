76cc29f864caec9a3ae712bbc26a9c4452ea60ac
Md.Rokunuzzaman <zaman3820@diu.edu.bd>
2020-04-18 04:52:42 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>slider</title>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<!--   header area-->
+    <div class="header_area">
+        <header>
+            <div class="header_left">
+                <a href="#"><img src="img/logo.png" alt="logo"></a>
+            </div>
+            <div class="header_right">
+
+            </div>
+        </header>
+    </div>
+<!--    meanu area-->
+    <div class="menu_area">
+        <nav>
+            <div class="menu">
+                <ul>
+                    <li><a class="active" href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
